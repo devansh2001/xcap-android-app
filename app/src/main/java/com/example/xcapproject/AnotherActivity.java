62e53ebeb5a396050ca2e3f3b67cb8bc3d23ab96
devansh2001 <dponda3@gatech.edu>
2020-08-01 23:24:33 -0400

    Tested dynamically adding UI components

--- app/src/main/java/com/example/xcapproject/AnotherActivity.java
+++ app/src/main/java/com/example/xcapproject/AnotherActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,6 +25,15 @@ public class AnotherActivity extends AppCompatActivity {
                 (HashMap<String, HashMap<Integer, ArrayList<AndroidPermissions>>>)
                 intent.getSerializableExtra(MainActivity.EXTRA_MESSAGE);
         Log.d(TAG, message.toString());
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
+        for (int i = 0; i < 10; i++) {
+            CheckBox btn = new CheckBox(this);
+            btn.setText("Work!");
+            layout.addView(btn);
+        }
+
+
 
     }
 }
