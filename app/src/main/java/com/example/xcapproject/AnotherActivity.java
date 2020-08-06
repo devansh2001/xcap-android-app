161499354326be9f09aeecfab9f120cd1c3704e1
devansh2001 <dponda3@gatech.edu>
2020-08-06 00:19:05 -0400

    sent data over to index.html on front-end

--- app/src/main/java/com/example/xcapproject/AnotherActivity.java
+++ app/src/main/java/com/example/xcapproject/AnotherActivity.java
@@ -2,17 +2,22 @@ package com.example.xcapproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 public class AnotherActivity extends AppCompatActivity {
 
@@ -36,11 +41,38 @@ public class AnotherActivity extends AppCompatActivity {
 //        }
 
         WebView view = (WebView) findViewById(R.id.webView);
+        view.setWebChromeClient(new WebChromeClient());
         view.setWebViewClient(new WebViewClient());
         view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl("https://www.google.com");
+        view.addJavascriptInterface(new WebAppInterface(this, message.keySet()), "Android");
+        view.loadUrl("http://localhost:3000");
 
 
 
     }
+}
+
+class WebAppInterface {
+    Context mContext;
+    Set<String> set;
+    public String TAG = "AnotherAct";
+    /** Instantiate the interface and set the context */
+    WebAppInterface(Context c, Set<String> set) {
+        mContext = c;
+        this.set = set;
+        Log.d(TAG, "GOT CONSTRUCTOR DATA");
+        Log.d(TAG, set.toString());
+    }
+
+    /** Show a toast from the web page */
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+
+    @JavascriptInterface
+    public String showKeySet() {
+        return set.toString();
+    }
+
 }
