c33ddfbf7740ba42d009cc4cecb8275ba4cfba4d
devansh2001 <dponda3@gatech.edu>
2020-10-22 22:48:49 -0400

    added participant id screen

---
+++ app/src/main/java/com/example/xcapproject/UserIdCollector.java
@@ -0,0 +1,42 @@
+package com.example.xcapproject;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserIdCollector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        findViewById(R.id.)
+        setContentView(R.layout.activity_user_id);
+
+        Button button = (Button) findViewById(R.id.userIdButton);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (sharedPreferences.contains("XCAP_UNIQUE_ID")) {
+            String id = sharedPreferences.getString("XCAP_UNIQUE_ID", "");
+            editText.setText(id);
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String userId = editText.getText().toString();
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("XCAP_UNIQUE_ID", userId);
+                editor.apply();
+
+                System.out.println(userId);
+            }
+        });
+    }
+}
